635796bdca821ebd816d0ebcc9eff63aabbe0aea
dprpic <inpendio@gmail.com>
2019-07-20 21:46:06 +0200

    wip

--- android/app/src/main/java/com/tubeseek/MainApplication.java
+++ android/app/src/main/java/com/tubeseek/MainApplication.java
@@ -3,6 +3,7 @@ package com.tubeseek;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.tanguyantoine.react.MusicControl;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import cl.json.RNSharePackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MusicControl(),
           new Interactable(),
             new RNFirebasePackage(),
             new RNSharePackage(),
